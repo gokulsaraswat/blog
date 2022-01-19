d8d1a80a850c0fc09238f6b9201d5235db5a802d
unknown <gokulsaraswat07@gmail.com>
2022-01-19 12:13:10 +0530

    UPdated little matters

--- data/authors/default.md
+++ data/authors/default.md
@@ -1,15 +1,15 @@
 ---
-name: Tails Azimuth
+name: Gokul Saraswat
 avatar: /static/images/avatar.png
-occupation: Professor of Atmospheric Science
-company: Stanford University
-email: address@yoursite.com
-twitter: https://twitter.com/Twitter
-linkedin: https://www.linkedin.com
-github: https://github.com
+occupation: Student of Information Science
+company: VTU University
+email: gokulsaraswat07@gmail.com
+twitter: https://twitter.com/gokulsaraswat
+linkedin: https://www.linkedin.com/gokulsaraswat
+github: https://github.com/gokulsaraswat
 ---
 
-Tails Azimuth is a professor of atmospheric sciences at the Stanford AI Lab. His research interests includes complexity modelling of tailwinds, headwinds and crosswinds.
+I am Student of Information Science at the BMSIT And .... His research interests includes complexity modelling of tailwinds, headwinds and crosswinds.
 
 He leads the clean energy group which develops 3D air pollution-climate models, writes differential equation solvers, and manufactures titanium plated air ballons. In his free time he bakes raspberry pi.
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -20,14 +20,41 @@ export default function Home({ posts }: InferGetStaticPropsType<typeof getStatic
   return (
     <>
       <PageSEO title={siteMetadata.title} description={siteMetadata.description} />
-      <div className="divide-y divide-gray-200 dark:divide-gray-700">
-        <div className="pt-6 pb-8 space-y-2 md:space-y-5">
-          <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
-            Latest
-          </h1>
-          <p className="text-lg leading-7 text-gray-500 dark:text-gray-400">
+      <div className=" divide-gray-200 dark:divide-gray-700">
+        {/* divide y must be the case for two hr */}
+        <div className="flex flex-col items-center my-6 xl:flex-row gap-x-12 xl:mb-12">
+          <div className="pt-6">
+            <h1 className="pb-6 text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">Hi, I’m Timothy Lin</h1>
+            <h2 className="text-lg prose text-gray-600 dark:text-gray-400">Welcome to my blog - Thoughts from a wandering mind. I am the co-founder of Cylynx, a datascientist by profession and economist by training. In my free time, I like developing <a href="/projects">side projects</a> and <a href="/blog">blogging</a> about them. Have a good read!</h2>
+          </div>
+          <div className="flex items-center justify-center mx-2 my-12 w-96">
+            <div className="flex items-center justify-center">
+              <div className="p-6 bg-gray-100 dark:bg-gray-800 sm:px-14 sm:py-8">
+                <div>
+                  <div className="pb-1 text-lg font-semibold text-gray-800 w-72 dark:text-gray-100">Stay updated, receive the latest post straight to your mailbox</div>
+                    <form className="flex flex-col">
+                      <div>
+                        <label className="sr-only" htmlFor="email-input">Email address</label>
+                        <input type="email" autoComplete="email" className="px-4 rounded-md w-72 dark:bg-black focus:outline-none focus:ring-2 focus:border-transparent focus:ring-primary-600" id="email-input" name="email" placeholder="Enter your email" ></input>
+                      </div>
+                      <div className="mt-2 rounded-md shadow-sm">
+                        <button className="py-2 w-72 bg-primary-500 px-4 rounded-md font-medium text-white hover:bg-primary-700 dark:hover:bg-primary-400 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-primary-600 dark:ring-offset-black" type="submit">Sign up</button>
+                      </div>
+                    </form>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+
+        <div className="pt-6 pb-2 space-y-2 md:space-y-5">
+        <h2 className="flex pb-2 text-2xl font-extrabold tracking-tight text-gray-900 dark:text-gray-100 sm:text-3xl md:text-5xl">
+          Latest</h2>
+          {/* <p className="text-lg leading-7 text-gray-500 dark:text-gray-400">
             {siteMetadata.description}
-          </p>
+          </p> */}
+          <hr class="border-gray-200 dark:border-gray-700"></hr>
         </div>
         <ul className="divide-y divide-gray-200 dark:divide-gray-700">
           {!posts.length && 'No posts found.'}
